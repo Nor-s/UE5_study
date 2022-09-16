1b08975e67bf51b60e7baae9fc496b80bc1d8962
nsg <40836757+Nor-s@users.noreply.github.com>
2022-09-16 21:54:14 +0900

    Add CH4

---
+++ Source/ArenaBattle/ABGameMode.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABGameMode.h"
+#include "ABPawn.h"
+#include "ABPlayerController.h"
+
+
+// 주의: 폰을 생성하고 지정하는 것이 아니라, 클래스 정보를 지정함
+// 멀티를 고려한다면, 미리 폰을 만들어두는 것이 아니라 클래스 정보만 저장, 플레이어 입장 시 생성하는 것이 합리적
+AABGameMode::AABGameMode()
+{
+	// 언리얼 오브젝트의 클래스 정보는 언리얼 헤더 툴에 의해 자동으로 생성, StaticClass() 함수 호출해 가져옴
+	DefaultPawnClass = AABPawn::StaticClass();
+	PlayerControllerClass = AABPlayerController::StaticClass();
+}
+
+void AABGameMode::PostLogin(APlayerController * NewPlayer)
+{
+	ABLOG(Warning, TEXT("PostLogin"));
+	Super::PostLogin(NewPlayer);
+	ABLOG(Warning, TEXT("PostLogin End"));
+}
--- Source/ArenaBattle/ArenaBattle.h
+++ Source/ArenaBattle/ArenaBattle.h
@@ -8,18 +8,3 @@ DECLARE_LOG_CATEGORY_EXTERN(ArenaBattle, Log, All);
 #define ABLOG_CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
 #define ABLOG_S(Verbosity) UE_LOG(ArenaBattle, Verbosity, TEXT("%s"), *ABLOG_CALLINFO)
 #define ABLOG(Verbosity, Format, ...) UE_LOG(ArenaBattle, Verbosity, TEXT("%s %s"), *ABLOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
-
-/**
-* CH4
-* - 게임을 만드는 작업 = 레벨을 구성하는 작업 + 게임플레이 설계하는 작업
-* - 게임 플레임워크: 게임의 규칙을 관리하는 게임 모드 + 플레이어가 조종하는 액터인 폰
-*
-* 게임 규칙
-* - 무형적인 요소
-* - 심판에 해당하는 역할
-*
-* 언리얼의 게임모드
-* - 게임의 틀을 잡아주는 역할
-* - 플레이어가 입장할 때마다 플레이어를 점검, 게임에 입장한 플레이어가 조종할 액터를 생성해 전달
-* - 플레이어가 조종할 수 있는 액터: 폰
-* 
