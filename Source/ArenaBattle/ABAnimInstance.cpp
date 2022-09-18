a308da8592fad1bf8d529533dd0b6ed1ae7e7298
nsg <40836757+Nor-s@users.noreply.github.com>
2022-09-19 02:07:04 +0900

    Add CH7 Anim Instance

---
+++ Source/ArenaBattle/ABAnimInstance.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABAnimInstance.h"
+
+
+UABAnimInstance::UABAnimInstance()
+{
+	CurrentPawnSpeed = 0.0f;
+}
+
+
+void UABAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	
+	if(const APawn* const Pawn = TryGetPawnOwner(); ::IsValid(Pawn))
+	{
+		CurrentPawnSpeed = Pawn->GetVelocity().Size();
+	}
+}
+
---
+++ Source/ArenaBattle/ABAnimInstance.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "ArenaBattle.h"
+#include "Animation/AnimInstance.h"
+#include "ABAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENABATTLE_API UABAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UABAnimInstance();
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	// 블루프린트에서 접근하는 키워드 == BlueprintReadOnly, BlueprintReadWrite
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Pawn, Meta=(AllowPrivateAccess=true))
+	float CurrentPawnSpeed;
+};
